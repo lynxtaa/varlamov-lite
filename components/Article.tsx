1684ca3eb8c5a486d092e1a62614f28a258ce106
Alex Kondratyuk <lynxtaa@gmail.com>
2020-12-21 10:03:21 +0300

    progress bar

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useMemo, Fragment } from 'react'
-import { ArrowLeft } from 'react-feather'
+import { Home } from 'react-feather'
 
 import styles from './Article.module.css'
 import Link from './Link'
@@ -40,7 +40,7 @@ export default function Article({ title, text }: Props) {
 		<article className={styles.Article}>
 			<div className={styles.backArrow}>
 				<Link href="/">
-					<ArrowLeft />
+					<Home />
 				</Link>
 			</div>
 			<Head>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
+import debounce from 'lodash.debounce'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Router } from 'next/router'
+import nprogress from 'nprogress'
 import { StrictMode } from 'react'
 
 import 'focus-visible'
 
 import '../styles/global.css'
+
+// Only show nprogress after 500ms (slow loading)
+// https://github.com/gaearon/whatthefuck.is/blob/24d3bfb4b6ed60d01414cb6295accd6f5d00d3b9/pages/_app.js
+const start = debounce(nprogress.start, 500)
+
+Router.events.on('routeChangeStart', start)
+
+Router.events.on('routeChangeComplete', () => {
+	start.cancel()
+	nprogress.done()
+	window.scrollTo(0, 0)
+})
+
+Router.events.on('routeChangeError', () => {
+	start.cancel()
+	nprogress.done()
+})
 
 const App = ({ Component, pageProps }: AppProps) => (
 	<StrictMode>
