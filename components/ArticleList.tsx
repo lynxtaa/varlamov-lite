c715d36007579eec305c502c2defad976e400abd
Alex Kondratyuk <lynxtaa@gmail.com>
2020-12-25 01:31:54 +0300

    better arrow styles

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -20,31 +20,39 @@ export default function ArticleList({ initialData }: Props) {
 
 	const pathname = tag ? `/tag/${encodeURIComponent(String(tag))}/` : '/'
 
-	const prevPage = `${pathname}page/${pageNum + 1}`
+	const prevPage = initialData.length > 0 ? `${pathname}page/${pageNum + 1}` : null
 	const nextPage =
 		pageNum > 1 ? (pageNum > 2 ? `${pathname}page/${pageNum - 1}` : pathname) : null
 
 	return (
 		<Page className={styles.ArticleList}>
 			{tag && <h1 className={styles.tag}>{tag}</h1>}
-			{initialData.map(article => (
-				<h3 key={article.id} className={styles.article}>
-					<Link href={`/blog/${article.id}`}>{article.title}</Link>
-				</h3>
-			))}
+			{initialData.length > 0 ? (
+				initialData.map(article => (
+					<h3 key={article.id} className={styles.article}>
+						<Link href={`/blog/${article.id}`}>{article.title}</Link>
+					</h3>
+				))
+			) : (
+				<h3 className={styles.article}>Ничего не найдено</h3>
+			)}
 			<div className={styles.arrows}>
-				<Link href={prevPage} title="Предыдущие статьи">
-					<Icon icon={<ArrowLeft />} size={1.7} />
-				</Link>
-				<span>{pageNum}</span>
-				{nextPage ? (
-					<Link href={nextPage} title="Следующие статьи">
-						<Icon icon={<ArrowRight />} size={1.7} />
+				{prevPage ? (
+					<Link href={prevPage} title="Предыдущая страница">
+						<Icon icon={<ArrowLeft />} size={1.7} />
 					</Link>
 				) : (
 					<span style={{ opacity: 0.5 }}>
-						<Icon icon={<ArrowRight />} size={1.7} />
+						<Icon icon={<ArrowLeft />} size={1.7} />
 					</span>
+				)}
+				{nextPage && (
+					<>
+						<span>{pageNum}</span>
+						<Link href={nextPage} title="Следующая страница">
+							<Icon icon={<ArrowRight />} size={1.7} />
+						</Link>
+					</>
 				)}
 			</div>
 		</Page>
