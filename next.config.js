ae31b07a4f77272b141e1506a6ce3768993ff2a5
Alex Kondratyuk <lynxtaa@gmail.com>
2021-01-01 22:49:02 +0300

    add new img domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	images: {
-		domains: ['varlamov.me', 'ic.pics.livejournal.com'],
+		domains: ['varlamov.me', 'www.varlamov.me', 'ic.pics.livejournal.com'],
 	},
 	async redirects() {
 		return [{ source: '/:id.html', destination: '/blog/:id', permanent: true }]
