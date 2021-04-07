a5ff4ba02775924b39fc288080dd1bc1891993be
Alex Kondratyuk <lynxtaa@gmail.com>
2021-04-07 18:07:23 +0300

    support more image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
 	images: {
-		domains: ['varlamov.me', 'www.varlamov.me', 'ic.pics.livejournal.com'],
+		domains: [
+			'varlamov.me',
+			'www.varlamov.me',
+			'ic.pics.livejournal.com',
+			'farm5.staticflickr.com',
+			'pp.userapi.com',
+		],
 	},
 	async redirects() {
 		return [{ source: '/:id.html', destination: '/blog/:id', permanent: true }]
