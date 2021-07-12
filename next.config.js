549819a0fc70abc753d04cd905732303c7ebfc51
Alex Kondratyuk <lynxtaa@gmail.com>
2021-07-12 21:45:17 +0300

    support more image domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
 			'ic.pics.livejournal.com',
 			'farm5.staticflickr.com',
 			'pp.userapi.com',
+			'teletype.in',
 			'img1.teletype.in',
 			'img2.teletype.in',
 			'img3.teletype.in',
@@ -16,9 +17,6 @@ module.exports = {
 			'img8.teletype.in',
 			'img9.teletype.in',
 		],
-	},
-	async redirects() {
-		return [{ source: '/:id.html', destination: '/blog/:id', permanent: true }]
 	},
 	async rewrites() {
 		return [{ source: '/feed.xml', destination: '/api/feed.xml' }]
