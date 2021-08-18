8fc1a5083a3bb45566c9a4d9f9a4bb7d9121c381
Alex Kondratyuk <lynxtaa@gmail.com>
2021-08-18 18:31:30 +0300

    update dependencies

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,6 +1,7 @@
 export default function Spinner() {
 	return (
 		<div className="flex justify-center items-center h-full">
+			{/* eslint-disable-next-line @next/next/no-img-element*/}
 			<img src="/logo.png" alt="Varlamov Face" className="max-h-60 animate-spinZ" />
 		</div>
 	)
