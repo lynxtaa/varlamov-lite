d09f4ae626824581e2ad068267ae305775a673b3
Alex Kondratiuk <lynxtaa@gmail.com>
2024-04-22 00:16:50 +0200

    handle error

--- app/Home.tsx
+++ app/Home.tsx
@@ -10,21 +10,37 @@ import { Article } from '../lib/varlamovClient'
 import { getArticles } from './actions/getArticles'
 
 export default function Home({ initialData }: { initialData: Article[] }) {
-	const [state, formAction] = useFormState<Article[]>(async state => {
-		const articles = await getArticles({ lastArticle: state.at(-1)?.id })
-		return [...state, ...articles]
-	}, initialData)
+	const [state, formAction] = useFormState<{
+		data: Article[]
+		error: string | undefined
+	}>(
+		async state => {
+			try {
+				const articles = await getArticles({ lastArticle: state.data.at(-1)?.id })
+				return { data: [...state.data, ...articles], error: undefined }
+			} catch (error) {
+				return {
+					data: state.data,
+					error: error instanceof Error ? error.message : String(error),
+				}
+			}
+		},
+		{ data: initialData, error: undefined },
+	)
 
 	const [pending, startTransition] = useTransition()
 
 	return (
 		<Page className="max-w-xl" isHome>
-			{state.length > 0 ? (
-				state.map(article => <ArticleListItem key={article.id} {...article} />)
-			) : (
-				<header className="mb-8">
-					<h2 className="mb-1 text-xl">Ничего не найдено</h2>
-				</header>
+			{state.data.length > 0
+				? state.data.map(article => <ArticleListItem key={article.id} {...article} />)
+				: state.error === undefined && (
+						<header className="mb-8">
+							<h2 className="mb-1 text-xl">Ничего не найдено</h2>
+						</header>
+					)}
+			{state.error !== undefined && (
+				<div className="mb-3">Ошибка загрузки: {state.error}</div>
 			)}
 
 			<button
