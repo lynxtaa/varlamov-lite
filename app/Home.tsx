056be4b1c938f1690199d9b0fd2f46ae10323ed3
Alex Kondratiuk <lynxtaa@gmail.com>
2024-05-10 00:16:37 +0200

    page

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,60 +1,26 @@
-'use client'
-
-import { useState } from 'react'
-import { useFormState } from 'react-dom'
-
 import ArticleListItem from '../components/ArticleListItem'
+import Link from '../components/Link'
 import Page from '../components/layouts/Page'
 import { Article } from '../lib/varlamovClient'
 
-import { getArticles } from './actions/getArticles'
-
 export default function Home({ initialData }: { initialData: Article[] }) {
-	const [isPending, setIsPending] = useState(false)
-
-	const [state, dispatch] = useFormState<{
-		data: Article[]
-		error: string | undefined
-	}>(
-		async state => {
-			try {
-				const articles = await getArticles({ lastArticle: state.data.at(-1)?.id })
-				return { data: [...state.data, ...articles], error: undefined }
-			} catch (error) {
-				return {
-					data: state.data,
-					error: error instanceof Error ? error.message : String(error),
-				}
-			} finally {
-				setIsPending(false)
-			}
-		},
-		{ data: initialData, error: undefined },
-	)
-
 	return (
 		<Page className="max-w-xl" isHome>
-			{state.data.length > 0
-				? state.data.map(article => <ArticleListItem key={article.id} {...article} />)
-				: state.error === undefined && (
-						<header className="mb-8">
-							<h2 className="mb-1 text-xl">Ничего не найдено</h2>
-						</header>
-					)}
-			{state.error !== undefined && (
-				<div className="mb-3">Ошибка загрузки: {state.error}</div>
+			{initialData.length > 0 ? (
+				initialData.map(article => <ArticleListItem key={article.id} {...article} />)
+			) : (
+				<header className="mb-8">
+					<h2 className="mb-1 text-xl">Ничего не найдено</h2>
+				</header>
 			)}
-
-			<button
-				className="text-sm py-2 px-3 border rounded border-gray-700 border-solid mb-3 hover:no-underline"
-				onClick={() => {
-					setIsPending(true)
-					dispatch()
-				}}
-				disabled={isPending}
-			>
-				Показать больше {isPending && '...'}
-			</button>
+			{initialData.at(-1) && (
+				<Link
+					href={`/?${new URLSearchParams({ lastArticle: String(initialData.at(-1)!.id) }).toString()}`}
+					className="text-sm py-2 px-3 border rounded border-gray-700 border-solid mb-3 hover:no-underline"
+				>
+					Назад
+				</Link>
+			)}
 		</Page>
 	)
 }
