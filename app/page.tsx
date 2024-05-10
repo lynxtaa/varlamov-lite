07d90a5065ddf41062464c7f0aa5cf174b8a80a8
Alex Kondratiuk <lynxtaa@gmail.com>
2024-05-10 18:36:27 +0200

    chore: use more server components (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { getArticles } from './actions/getArticles'
 
 export default async function Page() {
 	const articles = await getArticles({})
-	return <Home initialData={articles} />
+	return <Home initialData={articles} isFirstPage />
 }
